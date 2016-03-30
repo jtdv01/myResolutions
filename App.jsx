906021eddc8b1506c1c599bb1d522b9fb23d5e32
j <j@j.com>
2016-03-30 19:28:53 +1100

    Finished #3. Added first component

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-class App extends React.Component{
+export default class App extends React.Component{
   render(){
     return(
       <h1> Hello World </h1>
@@ -9,8 +9,8 @@ class App extends React.Component{
   }
 }
 
-if(Meteor.isClient){
-  Meteor.startup(function(){
-    React.render(<App />,document.getElementById('render-target'));
-  });
-}
+// if(Meteor.isClient){
+//   Meteor.startup(function(){
+//     React.render(<App />,document.getElementById('render-target'));
+//   });
+// }
--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
+npm init
 npm install --save react
+npm install --save react-dom
+npm install --save react-mounter
+meteor add kadira:flow-router
