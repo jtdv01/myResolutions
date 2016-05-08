9e16ca6255e60ed440e2f356cf655752a30e1d63
j <j@j.com>
2016-05-08 15:43:39 +1000

    Messy css

---
+++ README.md
@@ -0,0 +1,6 @@
+# Resolutions
+
+A Meteor app using React as the front end.
+
+Based on Meteor & React for Everyone by LevelUpTuts
+https://www.youtube.com/watch?v=B_HJCmoSvmc
---
+++ client/Resolution/ResolutionForm.js
@@ -0,0 +1,19 @@
+// Resolution = new Mongo.Collection('resolutions');
+
+// ResolutionsSchema = new SimpleSchema({
+//   createdAt: {
+//     type:Date,
+//     label:"Created At",
+//     autoValue:function(){
+//       return new Date();
+//     },
+//     autoform:{
+//       type:"hidden"
+//     }
+//   }
+// })
+//
+// Resolutions.attachSchema();
+
+// Then in blaze: using quickform
+// {{ > quickForm collection="resolutions"}}
