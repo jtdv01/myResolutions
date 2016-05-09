d2144a5983db8af09da35664ef2056d0fd829a23
j <j@j.com>
2016-05-09 10:32:12 +1000

    Removed insecure

--- client/Resolution/ResolutionAddForm.jsx
+++ client/Resolution/ResolutionAddForm.jsx
@@ -7,14 +7,21 @@ export default class ResolutionAddForm extends React.Component{
 
     var text = this.refs.resolution.value.trim();
 
-    Resolutions.insert({
-      text:text,
-      complete:false,
-      createdAt: new Date()
+    // Call the callback if it succeeds using an arrow function
+    // Arror functions change the context of this
+    // If you used function(){}, it will look for 'this'
+    // if you use arrow function, the 'this' will refer to the class? Resolution form
+    // rather than the function of Meteor.call
+
+    Meteor.call("addResolution",text,()=>{
+      this.refs.resolution.value="";
     });
 
-    this.refs.resolution.value = "";
-
+    /*
+    Now if you try to type this in the javascript console of the browserL:
+    Resolutions.insert({'a':1})
+    It will fail because we removed insecure
+    */
   }
 
   render(){
